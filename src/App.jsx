14f25b26f23bb29698abffe060ba4712b7fd52e5
Thien Thach <vnsnake123@gmail.com>
2023-07-25 09:34:47 +0700

    check di

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <div className="header">Text Decorator</div>
+      <div className="container max-w-5xl mx-auto mt-6">
+        <textarea name="" id="" cols="50" rows="10" className='w-full'></textarea>
+        <div className="columns-2">
+          <div className="w-full bg-blue-500">Thach</div>
+          <div className='w-full'>Danh</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
